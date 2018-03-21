a8b07702f45612621fe90abb097afade277618ec
sjnya <sjnyas@gmail.com>
2018-03-21 22:10:14 +0900

    try SeekBuffer

--- extensions/flac/src/main/java/com/google/android/exoplayer2/ext/flac/FlacDecoderJni.java
+++ extensions/flac/src/main/java/com/google/android/exoplayer2/ext/flac/FlacDecoderJni.java
@@ -16,13 +16,16 @@
 package com.google.android.exoplayer2.ext.flac;
 
 import android.util.Log;
+import android.util.Pair;
 
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.extractor.ExtractorInput;
 import com.google.android.exoplayer2.util.FlacStreamInfo;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * JNI wrapper for the libflac Flac decoder.
@@ -37,7 +40,77 @@ import java.util.Arrays;
   private ExtractorInput extractorInput;
   private boolean endOfExtractorInput;
   private byte[] tempBuffer;
-  private byte[] seekBuffer;
+  private SeekBuffer seekBuffer;
+
+  private class SeekBuffer{
+    private byte[] skippedBuffer;
+    private List<Pair<Integer, byte[]>> readBuffers;
+
+    public SeekBuffer(int size){
+      skippedBuffer = new byte[size];
+      readBuffers = new ArrayList<>();
+    }
+
+    public int length(){
+      return skippedLength() + readLength();
+    }
+
+    public byte[] read(int offset, int length){
+      if(offset < 0 || length() < offset) {
+        Log.e("FlacDecoderJni", "seekBuffer short error length: " + seekBuffer.length());
+        return new byte[0];
+      }
+      if(offset + length > length()){
+        length = length() - offset;
+      }
+      int byteCount = 0;
+      byte[] result = new byte[length];
+      int skippedLength = skippedLength();
+      if(offset < skippedLength){
+        int from = offset;
+        int to = Math.min(skippedLength, offset + length);
+        System.arraycopy(skippedBuffer, from, result,0, to - from);
+        //Log.e("FlacDecoderJni", "result from: " + 0 + ", length: " + (to - from));
+        byteCount += to - from;
+      }
+      if(byteCount >= length){
+        return result;
+      }
+      int position = skippedLength;
+      for(Pair<Integer, byte[]> pair : readBuffers){
+        if(position + pair.first < offset + byteCount){
+          position += pair.first;
+          continue;
+        }
+        int from = Math.max(offset - position, 0);
+        int to = Math.min(from + (length - byteCount), pair.first);
+        System.arraycopy(pair.second, from, result, byteCount,to - from);
+        //Log.e("FlacDecoderJni", "result from: " + byteCount + ", length: " + (to - from));
+        byteCount += to - from;
+        position += pair.first;
+        if(byteCount >= length){
+          return result;
+        }
+      }
+      return result;
+    }
+
+    public void addReadBuffer(int length, byte[] buffer) {
+      readBuffers.add(Pair.create(length, buffer));
+    }
+
+    private int skippedLength(){
+      return skippedBuffer != null ? skippedBuffer.length : 0;
+    }
+
+    private int readLength(){
+      int readLength = 0;
+      for(Pair<Integer, byte[]> buffer : readBuffers){
+        readLength += buffer.first;
+      }
+      return readLength;
+    }
+  }
 
   public FlacDecoderJni() throws FlacDecoderException {
     if (!FlacLibrary.isAvailable()) {
@@ -82,7 +155,6 @@ import java.util.Arrays;
   }
 
   public long getStreamLength(){
-    Log.e("FlacDecoderJni", "getStreamLength");
     if (extractorInput != null) {
       return extractorInput.getLength();
     } else {
@@ -114,21 +186,22 @@ import java.util.Arrays;
       byteBufferData.limit(originalLimit);
     } else if (extractorInput != null) {
       int skip = offset - ((int) extractorInput.getPosition());
-      Log.e("FlacDecoderJni", "read byteCount: " + byteCount + ", offset: " + offset + ", currentPos: " + (int) extractorInput.getPosition() + ", skip: " + skip);
+      if(seekBuffer != null)
+        //Log.e("FlacDecoderJni", "read byteCount: " + byteCount + ", offset: " + offset + ", currentPos: " + (int) extractorInput.getPosition() + ", skip: " + skip + ", length: " + seekBuffer.length());
       if(skip < 0){
-        if(seekBuffer.length + skip < 0){
-          return -1;
-        }
-        byte[] buffered = Arrays.copyOfRange(seekBuffer,seekBuffer.length + skip, seekBuffer.length + skip + Math.min(skip * -1, TEMP_BUFFER_SIZE));
+        byte[] buffered = seekBuffer.read(seekBuffer.length() + skip, byteCount);
         byteCount = readFromExtractorInput(0, Math.min(byteCount - buffered.length, TEMP_BUFFER_SIZE));
         byte[] result = new byte[buffered.length + byteCount];
         System.arraycopy(buffered,0, result,0, buffered.length);
         System.arraycopy(tempBuffer,0, result, buffered.length, byteCount);
         target.put(result, 0, buffered.length + byteCount);
+        Log.e("FlacDecoderJni", "read buffered: " + buffered.length + ", result: " + result.length + ", byteCount: " + byteCount);
         return result.length;
       }
-      seekBuffer = new byte[skip];
-      extractorInput.readFully(seekBuffer, 0, skip);
+      if(skip > TEMP_BUFFER_SIZE) {
+        seekBuffer = new SeekBuffer(skip);
+        extractorInput.readFully(seekBuffer.skippedBuffer, 0, skip);
+      }
       byteCount = Math.min(byteCount, TEMP_BUFFER_SIZE);
       int read = readFromExtractorInput(0, byteCount);
       if (read < 4) {
@@ -209,6 +282,8 @@ import java.util.Arrays;
     if (read == C.RESULT_END_OF_INPUT) {
       endOfExtractorInput = true;
       read = 0;
+    }else if(seekBuffer != null){
+      seekBuffer.addReadBuffer(read, tempBuffer);
     }
     return read;
   }
